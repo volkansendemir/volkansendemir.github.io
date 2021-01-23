a947274a0f5fea8066a2300b9ca9d286a42249ab
Volkan Sendemir <38820790+volkansendemir@users.noreply.github.com>
2021-01-23 17:17:29 +0300

    Create sign-up.js

---
+++ sitar/js/sign-up.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', function () {
+    const thisForm = document.getElementById('sign-up');
+    thisForm.addEventListener('submit', async function (e) {
+        e.preventDefault();
+        const formData = new FormData(thisForm).entries()
+        const response = await fetch("https://api.volkansendemir.com/sitar/login/sign-up", {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', 'x-api-key': 'sitar-test-input-api-key' },
+            body: JSON.stringify(Object.fromEntries(formData))
+        });
+
+        const result = await response.json();
+        window.localStorage.setItem('username', JSON.stringify(response["username"]));
+        window.localStorage.setItem('user-key', JSON.stringify(response["user-key"]));
+        window.localStorage.setItem('user-details', JSON.stringify(response["details"]));
+        window.localStorage.setItem('projects', JSON.stringify(response["projects"]));
+    });
+});
